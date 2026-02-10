9f9c36046882ef77046b75bdba8504090b8d4ea5
Renato de Oliveira Moreira <160731995+RenatoVoid@users.noreply.github.com>
2026-02-10 16:56:34 -0300

    Create memorias.js

---
+++ js/memorias.js
@@ -0,0 +1,11 @@
+// js/memorias.js
+const listaFotos = [
+    {
+        url: 'https://i.postimg.cc/FzwjL2LV/pulseira.jpg',
+        legenda: 'A pulseira que combinamos ✨'
+    },
+    {
+        url: 'https://link-da-proxima-foto.jpg',
+        legenda: 'Outro momento especial'
+    }
+];
